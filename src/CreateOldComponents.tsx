7b5c4abb74b1159925488629636441edca77f560
tian16300 <hubeitxq@126.com>
2018-03-22 22:28:46 +0800

    与jquery结合 以及第三方组件库结合

---
+++ src/CreateOldComponents.tsx
@@ -0,0 +1,19 @@
+import * as $  from 'jquery';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+let xml = `<Module key="TextBar"></Module>`;
+let xmlDoc = $.parseXML(xml) ;
+let childs = xmlDoc.childNodes ;
+let arr: Array<Element> = Array.prototype.slice.call( childs , 0 , childs.length);
+arr.forEach( (node: Element) => {
+    let name = node.tagName;
+    if ('Module' === name ) {
+       ReactDOM.render(<Module key="Textbar" /> , document.getElementById('root') as HTMLDivElement , () => {
+           alert('第三方库测试');           
+       });
+    }
+} );
+
+function Module( props: {key: string}) {
+    return <div  data-pKey={props.key} >组件测试</div>;
+}
